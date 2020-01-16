4766a44ffb842189c4a7293e0bfc176780aa0d65
Raymond Oung <r.oung@hapi-robo.com>
2020-01-16 10:16:07 +0900

    Adds unit tests to workflow and adds usage instructions to test script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,37 +1,66 @@
 #!/bin/sh
-# help user:
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 BUILD_FOLDER" >&2
-  exit 1
-fi
-if ! [ -e "$1" ]; then
-  echo "$1 not found" >&2
-  exit 1
-fi
-if ! [ -d "$1" ]; then
-  echo "$1 not a directory" >&2
-  exit 1
-fi
+#
+# Run all unit tests
+#
+# Usage
+#   sh run_tests.sh <path_to_build_directory>
+#
 
-# run all unit tests (except the manual ones)
-$1test/test_audiofile/test-audiofile
-status=$?
-$1test/test_beatdetect/test-beatdetect
-status=$(($status + $?))
-$1test/test_kissfft/test-kissfft
-status=$(($status + $?))
-$1test/test_primitives/test-primitives
-status=$(($status + $?))
-$1test/test_utils/test-utils
-status=$(($status + $?))
-
-# output result of all tests
+# constants
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
-if [ $status -eq 0 ]
-then
+
+# display usage instructions
+usage()
+{
+  echo ""
+  echo "usage: run_tests.sh <build-dir>"
+  echo ""
+  echo "Run all unit tests."
+  echo ""
+  echo "positional arguments:"
+  echo ""
+  echo "  build-dir             cmake build directory"
+  echo ""
+  exit 1
+}
+
+# check for build-directory
+if [ "$#" -ne 1 ]; then
+  echo "Incorrect number of arguments" >&2
+  usage
+fi
+
+if ! [ -e "$1" ]; then
+  echo "$1 not found" >&2
+  usage
+fi
+
+if ! [ -d "$1" ]; then
+  echo "$1 is not a directory" >&2
+  usage
+fi
+
+BUILD_DIR=$1
+
+# run all unit tests (except the manual ones)
+"${BUILD_DIR}/test/test_audiofile/test-audiofile"
+STATUS=$?
+"${BUILD_DIR}/test/test_beatdetect/test-beatdetect"
+STATUS=$(($STATUS + $?))
+"${BUILD_DIR}/test/test_kissfft/test-kissfft"
+STATUS=$(($STATUS + $?))
+"${BUILD_DIR}/test/test_primitives/test-primitives"
+STATUS=$(($STATUS + $?))
+"${BUILD_DIR}/test/test_utils/test-utils"
+STATUS=$(($STATUS + $?))
+
+# output result of all tests
+if [ $STATUS -eq 0 ]; then
   echo "${GREEN}ALL TESTS SUCCESSFUL${NC}"
+  exit 0
 else
-  echo "${RED}NOT ALL TESTS SUCCESSFUL, SEE ABOVE${NC}"
+  echo "${RED}NOT ALL TESTS SUCCESSFUL, SEE ABOVE${NC}" >&2
+  exit 1
 fi
