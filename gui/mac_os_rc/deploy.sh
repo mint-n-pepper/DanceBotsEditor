ca33d9110fb1af61755f01988457446537201e51
ray-hrst <44389573+ray-hrst@users.noreply.github.com>
2020-01-20 21:50:43 +0900

    Adds framework deployment to deploy.sh script

--- gui/mac_os_rc/deploy.sh
+++ gui/mac_os_rc/deploy.sh
@@ -1,9 +1,67 @@
 #!/bin/sh
-# create app directory and resources directory
-mkdir -p $1/DancebotsEditor.app/Contents/MacOS
-mkdir -p $1/DancebotsEditor.app/Contents/Resources
+#
+# Automatically creates an APK shortcut for temi's Launcher OS
+#
+# Usage
+#   ./shortcut.sh <apk> <shortcut-name>
+#
+
+# constants
+# should match CFBundleExecutable string in Info.plist
+APP_NAME="Dancebots Editor"
+
+# display usage instructions
+usage()
+{
+  echo ""
+  echo "usage: deploy.sh <build-dir> <qt-bin-dir> <qml-dir>"
+  echo ""
+  echo "Deploy app for macOS"
+  echo ""
+  echo "positional arguments:"
+  echo ""
+  echo "  build-dir             cmake build directory"
+  echo "  qt-bin-dir            QT bin directory"
+  echo "  qml-dir               QML directory"
+  echo ""
+  exit
+}
+
+# arguments
+# check for build directory
+if [ -z "$1" ]; then
+  echo "Missing build directory"
+  usage
+else
+  BUILD_DIR=$1
+fi
+
+# check for QT bin directory
+if [ -z "$2" ]; then
+  echo "Missing QT bin directory (absolute path)"
+  usage
+else
+  QT_BIN_DIR=$2
+fi
+
+# check for QML directory
+if [ -z "$3" ]; then
+  echo "Missing QML directory (absolute path)"
+  usage
+else
+  QML_DIR=$3
+fi
+
+
+# create app and resources directory
+mkdir -vp "${BUILD_DIR}/${APP_NAME}.app/Contents/MacOS"
+mkdir -vp "${BUILD_DIR}/${APP_NAME}.app/Contents/Resources"
 
 # copy executable, Info.plist, and Icon file
-cp -f $1/gui/dancebotsEditor $1/DancebotsEditor.app/Contents/MacOS/
-cp -f ./applogo.icns $1/DancebotsEditor.app/Contents/Resources/
-cp -f ./Info.plist $1/DancebotsEditor.app/Contents
+cp -vf "${BUILD_DIR}/gui/${APP_NAME}" "${BUILD_DIR}/${APP_NAME}.app/Contents/MacOS/"
+cp -vf ./applogo.icns "${BUILD_DIR}/${APP_NAME}.app/Contents/Resources/"
+cp -vf ./Info.plist "${BUILD_DIR}/${APP_NAME}.app/Contents"
+
+# copy the appropriate frameworks to the app
+cd "${BUILD_DIR}"
+"${QT_BIN_DIR}/macdeployqt" "./${APP_NAME}.app" -qmldir="${QML_DIR}" -dmg
