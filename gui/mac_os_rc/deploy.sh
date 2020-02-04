e834fa54a557d471621580fc32fe8fe389a76a88
ray-hrst <44389573+ray-hrst@users.noreply.github.com>
2020-02-04 20:49:47 +0900

    - Removes extra slash from argument auto-completion - Fixes deployment issue where the executable wasn't being properly copied to the resource directory, and renames the file appropriately

--- gui/mac_os_rc/deploy.sh
+++ gui/mac_os_rc/deploy.sh
@@ -27,13 +27,16 @@ usage()
   exit 1
 }
 
+# immediately exit if any command exits with a non-zero status
+set -e 
+
 # arguments
 # check for build directory
 if [ -z "$1" ]; then
   echo "Missing build directory"
   usage
 else
-  BUILD_DIR=$1
+  BUILD_DIR=${1%/}
 fi
 
 # check for QT bin directory
@@ -41,7 +44,7 @@ if [ -z "$2" ]; then
   echo "Missing QT bin directory (absolute path)"
   usage
 else
-  QT_BIN_DIR=$2
+  QT_BIN_DIR=${2%/}
 fi
 
 # check for QML directory
@@ -49,26 +52,29 @@ if [ -z "$3" ]; then
   echo "Missing QML directory (absolute path)"
   usage
 else
-  QML_DIR=$3
+  QML_DIR=${3%/}
 fi
 
 
 # create app and resources directory
+echo "Creating app and resources directory..."
 mkdir -vp "${BUILD_DIR}/${APP_NAME}.app/Contents/MacOS"
 mkdir -vp "${BUILD_DIR}/${APP_NAME}.app/Contents/Resources"
 
 # copy executable, Info.plist, and Icon file
-cp -vf "${BUILD_DIR}/gui/${APP_NAME}" "${BUILD_DIR}/${APP_NAME}.app/Contents/MacOS/"
+echo "Copying resources..."
+cp -vf "${BUILD_DIR}/gui/dancebotsEditor" "${BUILD_DIR}/${APP_NAME}.app/Contents/MacOS/${APP_NAME}"
 cp -vf ./applogo.icns "${BUILD_DIR}/${APP_NAME}.app/Contents/Resources/"
 cp -vf ./Info.plist "${BUILD_DIR}/${APP_NAME}.app/Contents"
 
 # copy the appropriate frameworks to the app and generate DMG file
+echo "Copying frameworks..."
 cd "${BUILD_DIR}"
 "${QT_BIN_DIR}/macdeployqt" "./${APP_NAME}.app" -qmldir="${QML_DIR}" -dmg
 
 # finish
-echo "Output"
+echo "Done"
+echo "App and DMG can be found at the following locations:"
 echo "${BUILD_DIR}/${APP_NAME}.app"
 echo "${BUILD_DIR}/${APP_NAME}.dmg"
-echo "Done"
 exit 0
